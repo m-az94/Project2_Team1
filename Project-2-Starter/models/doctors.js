b35b54f5b2c312ccb474b7f5ed8c18bcc2a2249a
m.az94@hotmail.com <maria@marias-MacBook-Air.local>
2019-05-09 19:47:20 -0400

    adding models

---
+++ Project-2-Starter/models/doctors.js
@@ -0,0 +1,18 @@
+// DOCTORS MODEL
+// This is the model used when a doctor is registering on the website
+//-----------------------------------------------------------------
+module.exports = function(sequelize, DataTypes) {
+  var  Doctors = sequelize.define("Doctor", {
+    name: DataTypes.STRING,
+    speciality: DataTypes.STRING,
+    location: DataTypes.STRING,
+    MIN: DataTypes.TEXT
+  });
+
+  Doctors.associate=function(models){
+    Doctors.hasMany(models.Patients,{
+      onDelete:"" //need to establish what to do with deleted doctor's patients
+    })
+  };
+  return Doctors;
+};
