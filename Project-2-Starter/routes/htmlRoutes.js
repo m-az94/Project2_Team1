204b1e7148deb009d4a9d7285b8e62a0334c3182
DMa <davidweibinma@Weibins-MacBook-Pro-2.local>
2019-05-11 16:04:34 -0400

    patients options handlebar

--- Project-2-Starter/routes/htmlRoutes.js
+++ Project-2-Starter/routes/htmlRoutes.js
@@ -4,7 +4,7 @@ var path = require("path");
 module.exports = function (app) {
   // Load index page
   app.get("/", function (req, res) {
-    res.render("patientProfile");
+    res.render("index");
   });
 
   //------------------------------------------------------------------
@@ -17,29 +17,32 @@ module.exports = function (app) {
   });
 
   // Doctor - sign in
-  app.get("/doctor/SignIn", function (req, res) {
+  app.get("/doctor/signin", function (req, res) {
     res.render("doctorSignIn");
   });
   // Doctor - sign up
-  app.get("/doctor/SignUp", function (req, res) {
+  app.get("/doctor/signup", function (req, res) {
     res.render("doctorSignUp");
   });
 
   // Doctor - create patient
-  app.get("/doctor/:id/createPatient", function (req, res) {
+  app.get("/doctor/:id/createpatient", function (req, res) {
     res.render("createPatient");
   });
   //------------------------------------------------------------------
   // PATIENT PAGES
   //------------------------------------------------------------------
-  app.get("/patient/SignIn", function (req, res) {
+  app.get("/patient/signin", function (req, res) {
     res.render("patientSignIn");
+  });
+
+  app.get("/patient/:id/options", function (req, res) {
+    var testId = { userFirstName: "test user" };
+    res.render("patientOptions", testId);
   });
 
   // Render 404 page for any unmatched routes
   app.get("*", function (req, res) {
     res.render("404");
   });
-
-
 };
