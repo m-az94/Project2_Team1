adef24a697a2d310570ba176adbace48b2c1678a
DMa <davidweibinma@Weibins-MacBook-Pro-2.local>
2019-05-12 17:06:03 -0400

    added seeds to seeds.sql

--- Project-2-Starter/models/doctors.js
+++ Project-2-Starter/models/doctors.js
@@ -2,14 +2,14 @@
 // This is the model used when a doctor is registering on the website
 //-----------------------------------------------------------------
 module.exports = function(sequelize, DataTypes) {
-  var  Doctors = sequelize.define("Doctors", {
+  var Doctors = sequelize.define("Doctors", {
     name: DataTypes.STRING,
     speciality: DataTypes.STRING,
     city: DataTypes.STRING,
     MINC: DataTypes.TEXT
   });
 
-  Doctors.associate=function(models){
+  Doctors.associate = function(models){
     Doctors.hasMany(models.Patients,{
       onDelete:"" //need to establish what to do with deleted doctor's patients
     })
