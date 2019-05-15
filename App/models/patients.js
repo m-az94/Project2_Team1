30acbf8992a1b876db245b45a5371ced46a6fe23
m.az94@hotmail.com <maria@marias-MacBook-Air.local>
2019-05-15 15:36:12 -0400

    created js files for public-js

---
+++ App/models/patients.js
@@ -0,0 +1,51 @@
+// PATIENTS MODEL
+// This is the model used when the doctor is creating a patient profile 
+//-----------------------------------------------------
+module.exports = function(sequelize, DataTypes) {
+    var  Patients = sequelize.define("Patients", {
+      id:{
+        autoIncrement: true,
+        primaryKey:true,
+        type: DataTypes.INTEGER
+      },
+      fullname: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      doctor_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+      phone: {
+        type: DataTypes.STRING,
+        allowNull:false
+      },
+      email:{
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate:{
+          isEmail: true
+        }
+      },
+      healthCard: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      pword:{
+        type: DataTypes.STRING,
+        allowNull: false
+      }
+    });
+  
+    Patients.associate=function(models){
+      Patients.belongsTo(models.Doctors,{
+        foreignKey: {
+            allowNull: false
+        }
+      });
+      Patients.hasMany(models.Prescriptions,{
+        onDelete: "cascade" //need to figure out what to do
+      });
+    };
+    return Patients;
+};
